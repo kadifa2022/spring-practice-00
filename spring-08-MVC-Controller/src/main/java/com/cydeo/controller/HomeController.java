67a2a7da4a5f1cfb9779361de47d601205bf2a40
Kadifa <Kudickadifa@gmail.com>
2023-05-12 14:53:39 -0500

    practice MVC

--- spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
+++ spring-08-MVC-Controller/src/main/java/com/cydeo/controller/HomeController.java
@@ -6,12 +6,28 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HomeController {
 
-    // in controller class  always String
+    // in controller class  always String// because it works with html.
     @RequestMapping("/home")
     public String home(){
         return "home.html";
 
     }
+    @RequestMapping("/ozzy")
+    public String getHomePage(){
+        return "home.html";
+
+    }
+    @RequestMapping("/")
+    public String getHomePage2(){
+        return "home.html";
+
+    }
+//    @RequestMapping("/student")
+//    public String getStudentInfo(){
+//        return "student/welcome.html";
+//
+//
+//    }
 
 
 
---
+++ spring-09-MVC-Model/src/main/java/com/cydeo/controller/StudentController.java
@@ -0,0 +1,14 @@
+package com.cydeo.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class StudentController {
+
+
+    public String homePage(){
+        return "Home";
+    }
+
+
+}
