3090d0d6efe44348f6694adbaad88492c25ca650
Kadifa <Kudickadifa@gmail.com>
2023-05-16 18:46:05 -0500

    practice Query

--- lab-08-api/src/main/java/com/cydeo/entity/Product.java
+++ lab-08-api/src/main/java/com/cydeo/entity/Product.java
@@ -1,27 +1,30 @@
 package com.cydeo.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.JoinColumn;
-import javax.persistence.JoinTable;
-import javax.persistence.ManyToMany;
+
 import java.math.BigDecimal;
 import java.util.List;
 
 @Entity
 @Getter
 @Setter
+@NoArgsConstructor
 public class Product extends BaseEntity{
     private BigDecimal price;
     private Integer quantity;
     private Integer remainingQuantity;
     private String name;
     @ManyToMany
-    @JoinTable(name = "product_category_rel",
+    @JoinTable(name ="product_category_rel",
             joinColumns = @JoinColumn(name="p_id"),
-            inverseJoinColumns = @JoinColumn(name = "c_id"))
+            inverseJoinColumns = @JoinColumn(name ="c_id"))
     private List<Category> categoryList;
 
 }
