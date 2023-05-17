8343c9bfe36dcb4aeeb8eeaf218098dda1658b11
Kadifa <Kudickadifa@gmail.com>
2023-05-17 10:17:52 -0500

    practice Query

--- lab-08-api/src/main/java/com/cydeo/service/serviceImp/ProductServiceImp.java
+++ lab-08-api/src/main/java/com/cydeo/service/serviceImp/ProductServiceImp.java
@@ -19,7 +19,7 @@ public class ProductServiceImp implements ProductService {
 
     private final ProductRepository productRepository;
     private final MapperUtil mapperUtil;
-    private final CategoryService categoryService;//
+    private final CategoryService categoryService;//we can not inject another repository inside the service implementation
 
     public ProductServiceImp(ProductRepository productRepository, MapperUtil mapperUtil, CategoryService categoryService) {
         this.productRepository = productRepository;
@@ -78,68 +78,17 @@ public class ProductServiceImp implements ProductService {
                 .collect(Collectors.toList());
     }
 
-
-}
-/*
     @Override
-    public ProductDTO updateProduct(ProductDTO productDTO) {
-        Product product = productRepository.save(mapperUtil.convert(productDTO, new Product()));
-        return mapperUtil.convert(product, new ProductDTO());
-    }
-
-    @Override
-    public ProductDTO createProduct(ProductDTO productDTO) {
-        Product product = productRepository.save(mapperUtil.convert(productDTO, new Product()));
-        return mapperUtil.convert(product, new ProductDTO());
-    }
-
-    @Override
-    public List<ProductDTO> retrieveProductByCategoryAndPrice(List<Long> categoryList, BigDecimal price) {
-        return productRepository.retrieveProductListByCategory(categoryList, price).stream()
-                .map(product -> mapperUtil.convert(product, new ProductDTO()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public ProductDTO retrieveProductByName(String name) {
-        return mapperUtil.convert(productRepository.findFirstByName(name), new ProductDTO());
-    }
-
-    @Override
-    public List<ProductDTO> retrieveProductByTop3ProductByPrice() {
-        return productRepository.findTop3ByOrderByPriceDesc().stream()
-                .map(product -> mapperUtil.convert(product, new ProductDTO()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public Integer countProductByPrice(BigDecimal price) {
-        return productRepository.countProductByPriceGreaterThan(price);
-    }
-
-    @Override
-    public List<ProductDTO> retrieveProductByPriceAndQuantity(BigDecimal price, Integer quantity) {
-        return productRepository.retrieveProductListGreaterThanPriceAndLowerThanRemainingQuantity(price, quantity)
-                .stream().map(product -> mapperUtil.convert(product, new ProductDTO()))
-                .collect(Collectors.toList());
-    }
-
-    @Override
-    public List<ProductDTO> retrieveProductByCategoryId(Long categoryId) {
+    public List<ProductDTO> retrieveByCategory(Long categoryId) {
         return productRepository.retrieveProductListByCategory(categoryId)
-                .stream().map(product -> mapperUtil.convert(product, new ProductDTO()))
+                .stream()
+                .map(product -> mapperUtil.convert(product,new ProductDTO()))
                 .collect(Collectors.toList());
     }
 
 
-
-
-
 }
 
-
-
- */
 
 
 
