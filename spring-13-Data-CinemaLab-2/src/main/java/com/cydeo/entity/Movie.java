8192e21227ff06bae291c51516bfebe7e1b6f43d
Kadifa <Kudickadifa@gmail.com>
2023-05-31 13:54:58 -0500

    practice

--- spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/entity/Movie.java
+++ spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/entity/Movie.java
@@ -5,12 +5,10 @@ import com.cydeo.enums.MovieType;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.EnumType;
-import javax.persistence.Enumerated;
+import javax.persistence.*;
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import java.util.List;
 
 @Entity
 @Data
@@ -28,4 +26,10 @@ public class Movie extends BaseEntity{
     @Enumerated(EnumType.STRING)
     private MovieType type;
     private BigDecimal price;
-}
+    //creating 3rd table
+    @ManyToMany
+    @JoinTable(name = "movie_genre_rel",
+    joinColumns = @JoinColumn(name = "movie_id"),
+    inverseJoinColumns = @JoinColumn(name = "genre_id"))
+    private List<Genre> genreList; //if is many to many use List<> and will be third table
+}// we can use set (because behind scene delete only one book and list delete all books and insert they again w/o  particular book
