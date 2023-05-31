13fb462776db560b7b53f139f1f795d90e9bca57
Kadifa <Kudickadifa@gmail.com>
2023-05-30 22:23:53 -0500

    practice

---
+++ spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/Spring13DataCinemaLab2Application.java
@@ -0,0 +1,13 @@
+package com.cydeo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Spring13DataCinemaLab2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Spring13DataCinemaLab2Application.class, args);
+	}
+
+}
