dd8b970590c4397ed194aa12bfbcb517ce2278db
Kadifa <Kudickadifa@gmail.com>
2023-06-05 11:55:40 -0500

    practice Spring data

---
+++ ticketing-project-data/src/main/java/com/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.repository;
+
+import com.cydeo.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+
+
+    User findByUserName(String username);
+
+}
