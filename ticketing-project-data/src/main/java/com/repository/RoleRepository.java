fb78c0c808f58196baf05e9d0cad7ae2671ddda4
Kadifa <Kudickadifa@gmail.com>
2023-06-05 15:53:55 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/repository/RoleRepository.java
+++ ticketing-project-data/src/main/java/com/repository/RoleRepository.java
@@ -2,10 +2,10 @@ package com.repository;
 
 import com.cydeo.entity.Role;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
 
-@Repository
+
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
 
     // give me role base on description
