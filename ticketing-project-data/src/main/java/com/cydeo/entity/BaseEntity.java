35473e206c9203fad6840ee9e53a14ea5af1a4fe
Kadifa <Kudickadifa@gmail.com>
2023-06-04 21:51:02 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
+++ ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,18 +1,24 @@
 package com.cydeo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.MappedSuperclass;
+import lombok.*;
 
 import java.time.LocalDateTime;
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
+@MappedSuperclass
 public class BaseEntity {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // will let postgres to create primary key
     private Long id;
     private LocalDateTime insertDateTime;
-    private Long insertUserId;
+    private Long insertUserId;//by whom created
     private LocalDateTime lastUpdateTime;
     private Long lastUpdateUserId;
 }
