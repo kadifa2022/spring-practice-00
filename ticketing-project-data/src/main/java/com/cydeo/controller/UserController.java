4da32ff11ced5a14ec0ace85e5509deb6b6e5746
Kadifa <Kudickadifa@gmail.com>
2023-06-06 12:47:57 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/controller/UserController.java
+++ ticketing-project-data/src/main/java/com/cydeo/controller/UserController.java
@@ -6,11 +6,7 @@ import com.cydeo.service.UserService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.annotation.Validated;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @RequestMapping("user")
@@ -40,4 +36,35 @@ public class UserController {
         userService.save(user);
         return "redirect:/user/create";
     }
+
+    @GetMapping("/update/{username}")
+    public String editUser(@PathVariable("username") String username, Model model){
+
+        model.addAttribute("user", userService.findByUserName(username));
+        model.addAttribute("roles", roleService.listAllRoles());
+        model.addAttribute("users", userService.listAllUsers());
+
+        return "/user/update";
+    }
+
+    @PostMapping("/update")
+    public String updateUser(@ModelAttribute("user") UserDTO user, BindingResult bindingResult, Model model){
+
+        if(bindingResult.hasErrors()){
+
+            model.addAttribute("roles", roleService.listAllRoles());
+            model.addAttribute("users", userService.listAllUsers());
+            return "/user/update";
+        }
+        userService.update(user);
+        return "redirect:/user/update";
+    }
+
+    @GetMapping("/delete/{username}")
+    public String deleteUser(@PathVariable("username") String username){
+        //userService.deleteByUserName(username);
+        userService.delete(username);
+        return"redirect:/user/create";
+    }
+
 }
