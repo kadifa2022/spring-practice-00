0ade227d963c4750c3f36d66204097f904bbd6f6
Kadifa <Kudickadifa@gmail.com>
2023-06-06 16:06:35 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/converter/ProjectDtoConvertor.java
+++ ticketing-project-data/src/main/java/com/cydeo/converter/ProjectDtoConvertor.java
@@ -2,10 +2,12 @@ package com.cydeo.converter;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.service.ProjectService;
+import org.springframework.boot.context.properties.ConfigurationPropertiesBinding;
 import org.springframework.core.convert.converter.Converter;
 import org.springframework.stereotype.Component;
 
 @Component
+//@ConfigurationPropertiesBinding
 public class ProjectDtoConvertor implements Converter<String, ProjectDTO> {
 
     ProjectService projectService;
