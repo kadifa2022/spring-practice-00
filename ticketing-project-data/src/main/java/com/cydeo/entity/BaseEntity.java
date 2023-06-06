82d78144edcff271381cad035810d64157a1a433
Kadifa <Kudickadifa@gmail.com>
2023-06-06 15:46:40 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
+++ ticketing-project-data/src/main/java/com/cydeo/entity/BaseEntity.java
@@ -1,9 +1,6 @@
 package com.cydeo.entity;
 
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.*;
 
 import java.time.LocalDateTime;
@@ -18,8 +15,25 @@ public class BaseEntity {
     @GeneratedValue(strategy = GenerationType.IDENTITY) // will let postgres to create primary key
     private Long id;
     private  Boolean isDeleted = false;
+    @Column(nullable = false, updatable = false)// we don't want to see null in DB Keep insert time
     private LocalDateTime insertDateTime;
+    @Column(nullable = false, updatable = false)
     private Long insertUserId;//by whom created
-    private LocalDateTime lastUpdateTime;
+    @Column(nullable = false)
+    private LocalDateTime lastUpdateDateTime;
+    @Column(nullable = false)
     private Long lastUpdateUserId;
+
+    @PrePersist//this method will execute whatever we want to save, create, who inserted manager, admin..
+    private void onPrePersist(){
+        this.insertDateTime = LocalDateTime.now();
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.insertUserId = 1L;
+        this.lastUpdateUserId = 1L;
+    }
+    @PreUpdate
+    private void onPreUpdate(){//this method will execute whatever we want to update
+        this.lastUpdateDateTime = LocalDateTime.now();
+        this.lastUpdateUserId = 1L;
+    }
 }
