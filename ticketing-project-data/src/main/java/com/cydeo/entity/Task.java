1cec4ab31b2f1bb1f9383de78bc4c203e4e59589
Kadifa <Kudickadifa@gmail.com>
2023-06-07 14:56:24 -0500

    practice Spring data

---
+++ ticketing-project-data/src/main/java/com/cydeo/entity/Task.java
@@ -0,0 +1,37 @@
+package com.cydeo.entity;
+
+import com.cydeo.enums.Status;
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.hibernate.annotations.Where;
+
+import java.time.LocalDate;
+
+@Entity
+@NoArgsConstructor
+@Getter
+@Setter
+@Table(name = "tasks")
+@Where(clause = "is_deleted_false")
+public class Task extends BaseEntity{
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "project_id")
+    private Project project;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "assigned_employee_id")
+    private User assignedEmployee;
+
+    private String taskSubject;
+    private String taskDetail;
+
+    @Enumerated(EnumType.STRING)
+    private Status taskStatus;
+
+    @Column(columnDefinition = "DATE")
+    private LocalDate assignDate;
+
+}
