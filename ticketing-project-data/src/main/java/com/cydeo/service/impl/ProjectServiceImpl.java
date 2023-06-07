50005958fe01e7006bef8f6010b211e09bfa5db9
Kadifa <Kudickadifa@gmail.com>
2023-06-07 12:08:23 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
+++ ticketing-project-data/src/main/java/com/cydeo/service/impl/ProjectServiceImpl.java
@@ -2,6 +2,7 @@ package com.cydeo.service.impl;
 
 import com.cydeo.dto.ProjectDTO;
 import com.cydeo.entity.Project;
+import com.cydeo.enums.Status;
 import com.cydeo.mapper.ProjectMapper;
 import com.cydeo.repository.ProjectRepository;
 import com.cydeo.service.ProjectService;
@@ -24,28 +25,59 @@ public class ProjectServiceImpl implements ProjectService {
 
     @Override
     public ProjectDTO getByProjectCode(String code) {
-        return null;
+        Project project = projectRepository.findByProjectCode(code);
+        return projectMapper.convertToDto(project);
     }
 
     @Override
     public List<ProjectDTO> listAllProjects() {
-        List<Project> list = projectRepository.findAll(Sort.by("project code"));// got project from DB
+        List<Project> list = projectRepository.findAll(Sort.by("project code"));// got project
+        // from DB
         return list.stream().map(projectMapper::convertToDto).collect(Collectors.toList());
 
     }
 
     @Override
     public void save(ProjectDTO dto) {
+        dto.setProjectStatus(Status.OPEN);
+        Project project = projectMapper.convertToEntity(dto);
+        projectRepository.save(project);
+
 
     }
 
     @Override
     public void update(ProjectDTO dto) {
+        // get the dto based on  project code from DB
+        Project project = projectRepository.findByProjectCode(dto.getProjectCode());
+        // get the dto and converted to entity
+        Project convertedProject = projectMapper.convertToEntity(dto);
+        // getting id from project and setting to converted project
+        convertedProject.setId(project.getId());
+        //setting the project status
+        convertedProject.setProjectStatus(project.getProjectStatus());
+        // saving the converted project in DB
+        projectRepository.save(convertedProject);
 
     }
 
     @Override
     public void delete(String code) {
+        Project project = projectRepository.findByProjectCode(code);
+        project.setIsDeleted(true);// changing the field to true
+        projectRepository.save(project);
 
     }
+
+    @Override
+    public void complete(String projectCode) {
+        //get the project from DB by projectcode
+        Project project = projectRepository.findByProjectCode(projectCode);
+        project.setProjectStatus(Status.COMPLETE);// Set the status
+        projectRepository.save(project); // save the project
+
+
+    }
+
+
 }
