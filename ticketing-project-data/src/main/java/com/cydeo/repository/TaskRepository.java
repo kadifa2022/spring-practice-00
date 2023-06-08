f26cc8ec4afb147e71cbd6e35920c53b1bd56cac
Kadifa <Kudickadifa@gmail.com>
2023-06-08 16:24:50 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/repository/TaskRepository.java
+++ ticketing-project-data/src/main/java/com/cydeo/repository/TaskRepository.java
@@ -1,17 +1,30 @@
 package com.cydeo.repository;
 
+
+import com.cydeo.entity.Project;
 import com.cydeo.entity.Task;
+import com.cydeo.entity.User;
+import com.cydeo.enums.Status;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface TaskRepository extends JpaRepository<Task,Long > {
+import java.util.List;
+
+public interface TaskRepository extends JpaRepository<Task, Long> {//JPQL Query works with entity
+
     @Query("SELECT COUNT(t) FROM Task t WHERE t.project.projectCode = ?1 AND t.taskStatus <> 'COMPLETE'")
-    int totalCompletedTasks(String projectCode);
+    int totalNonCompletedTasks(String projectCode);
+
     @Query(value = "SELECT COUNT(*) " +
             "FROM tasks t JOIN projects p ON t.project_id = p.id " +
 
             "WHERE p.project_code=?1 AND t.task_status = 'COMPLETE'", nativeQuery = true)
-    int totalNonCompletedTasks(String projectCode);
+    int totalCompletedTasks(String projectCode);
 
 
+    List<Task> findAllByProject(Project project);
+
+    List<Task> findAllByTaskStatusIsNotAndAssignedEmployee(Status status, User user);
+
+    List<Task> findAllByTaskStatusAndAssignedEmployee(Status status, User user);
 }
