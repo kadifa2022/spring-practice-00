7c7c203ee2182cca04e04fdfa72a9f684adb3121
Kadifa <Kudickadifa@gmail.com>
2023-06-08 16:24:59 -0500

    practice Spring data

--- ticketing-project-data/src/main/java/com/cydeo/service/TaskService.java
+++ ticketing-project-data/src/main/java/com/cydeo/service/TaskService.java
@@ -1,6 +1,8 @@
 package com.cydeo.service;
 
+import com.cydeo.dto.ProjectDTO;
 import com.cydeo.dto.TaskDTO;
+import com.cydeo.enums.Status;
 
 import java.util.List;
 
@@ -14,4 +16,12 @@ public interface TaskService {
 
      int totalNonCompletedTask(String projectCode);
      int totalCompletedTask(String projectCode);
+
+     void deleteByProject(ProjectDTO projectDTO);
+
+    void completeByProject(ProjectDTO projectDTO);
+
+    List<TaskDTO> listAllTasksByStatusIsNot(Status status);
+    List<TaskDTO> listAllTasksByStatus(Status status);
+
 }
