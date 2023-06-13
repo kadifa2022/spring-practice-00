7f57d1bfd4d28f8a1355be70b5286f569b820e26
Kadifa <Kudickadifa@gmail.com>
2023-06-13 18:41:21 -0500

    practice Spring api

---
+++ spring-15-REST/src/main/java/com/cydeo/service/CourseService.java
@@ -0,0 +1,17 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.CourseDTO;
+
+import java.util.List;
+
+public interface CourseService {
+
+    CourseDTO createCourse(CourseDTO courseDTO);
+    CourseDTO getCourseById(Long courseId);
+    List<CourseDTO> getCoursesByCategory(String category);
+    List<CourseDTO> getCourses();
+
+    void updateCourse(Long courseId, CourseDTO courseDTO);
+    void deleteCourseById(Long courseId);
+    void deleteCourse();
+}
