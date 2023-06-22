5bce21a0ede715beb655c17910a322e0135f4388
Kadifa <Kudickadifa@gmail.com>
2023-06-21 21:42:13 -0500

    practice Spring

---
+++ lab-08-ecomerce-rest-api/src/main/java/com/cydeo/service/impl/ProductServiceImpl.java
@@ -0,0 +1,30 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.ProductDTO;
+import com.cydeo.entity.Product;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.ProductRepository;
+import com.cydeo.service.ProductService;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
+public class ProductServiceImpl  implements ProductService{
+
+    private final ProductRepository productRepository;
+    private final MapperUtil mapperUtil;
+
+    public ProductServiceImpl(ProductRepository productRepository, MapperUtil mapperUtil) {
+        this.productRepository = productRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+
+    @Override
+    public List<ProductDTO> retrieveListProduct() {
+        return productRepository.findAll().stream()
+                .map(product -> mapperUtil.convert(product, new ProductDTO())).collect(Collectors.toList());
+    }
+}
