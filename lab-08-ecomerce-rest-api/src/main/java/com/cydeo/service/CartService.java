fbea4d264a4dee236b0c7bcec0549cb98533a249
Kadifa <Kudickadifa@gmail.com>
2023-06-29 14:17:20 -0500

    practice Spring

---
+++ lab-08-ecomerce-rest-api/src/main/java/com/cydeo/service/CartService.java
@@ -0,0 +1,7 @@
+package com.cydeo.service;
+
+import com.cydeo.dto.CartDTO;
+
+public interface CartService {
+   public CartDTO findById(Long id);
+}
---
+++ lab-08-ecomerce-rest-api/src/main/java/com/cydeo/service/impl/CartServiceImpl.java
@@ -0,0 +1,26 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.dto.CartDTO;
+import com.cydeo.mapper.MapperUtil;
+import com.cydeo.repository.CartRepository;
+import com.cydeo.service.CartService;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CartServiceImpl implements CartService {
+
+
+    private final CartRepository cartRepository;
+
+    private final MapperUtil mapperUtil;
+
+    public CartServiceImpl(CartRepository cartRepository, MapperUtil mapperUtil) {
+        this.cartRepository = cartRepository;
+        this.mapperUtil = mapperUtil;
+    }
+
+    @Override
+    public CartDTO findById(Long id) {
+        return mapperUtil.convert(cartRepository.findById(id), new CartDTO());
+    }
+}
