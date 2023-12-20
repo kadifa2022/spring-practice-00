9183a58a4ec8918e49ec120cf2901be96cc885d7
Kadifa <Kudickadifa@gmail.com>
2023-12-20 15:39:15 -0600

    Practice api

---
+++ lab-08-rest/src/main/java/com/cydeo/lab08rest/entity/Address.java
@@ -0,0 +1,18 @@
+package com.cydeo.lab08rest.entity;
+import javax.persistence.*;
+
+import lombok.Getter;
+import lombok.Setter;
+
+
+
+@Entity
+@Getter
+@Setter
+public class Address extends BaseEntity{
+    private String name;
+    private String zipCode;
+    private String street;
+    @ManyToOne
+    private Customer customer;
+}
