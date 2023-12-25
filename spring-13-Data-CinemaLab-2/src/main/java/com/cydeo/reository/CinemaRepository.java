0f066c4fa90ad0d2a406518280d6c8e9fd9675bb
Kadifa <Kudickadifa@gmail.com>
2023-12-25 10:59:49 -0600

    Cinema Repository SQL completed

--- spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/reository/CinemaRepository.java
+++ spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/reository/CinemaRepository.java
@@ -3,6 +3,8 @@ package com.cydeo.reository;
 import com.cydeo.entity.Cinema;
 import com.cydeo.entity.Location;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 import java.util.Optional;
@@ -24,6 +26,26 @@ public interface CinemaRepository extends JpaRepository<Cinema, Long> {
 
     //----------------------JPQL QUERIES------------------------//
     //Write a JPQL query to read the cinema name with a specific id
+    @Query("SELECT c.name FROM Cinema c Where c.id =?1")
+    String  fetchCinemaById(@Param("id") Long id);
 
     //-----------------------Native Queries-------------------//
+
+    //Write a native query to read all cinema by location Country
+    @Query(value = "SELECT * FROM cinema c JOIN location l " +
+            "ON c.location_id = l.id WHERE l.country = ?1", nativeQuery = true)
+    List<Cinema> retrieveAllBasedOnLocationCountry(@Param("locationCountry") String locationCountry);
+
+    //Write a native query to read all cinemas by name or sponsor name contains a specific pattern
+    @Query(value = "SELECT * FROM cinema WHERE name ILIKE Lconcat ('%',?1,'%') " +
+            "OR sponsor_name ILIKE concat('%',?1,'%')", nativeQuery = true)
+    List<Cinema> retrieveAlLByNameOrSponsorName(@Param("pattern") String pattern);
+
+    // Write a native query to sort all cinemas by name
+    @Query(value = "SELECT  * FROM cinema ORDER BY name", nativeQuery = true)
+    List<Cinema> sortByName();
+
+    //Write a native query to distinct all cinemas by sponsor name
+    @Query(value = "SELECT DISTINCT sponsored_name FROM cinema", nativeQuery = true)
+    List<String> distinctBySponsoredName();
 }
