99e3cbaaf20427896652d07fc229e577b8df7789
Kadifa <Kudickadifa@gmail.com>
2023-12-25 11:17:56 -0600

    Genre Repository SQL completed

---
+++ spring-13-Data-CinemaLab-2/src/main/java/com/cydeo/reository/GenreRepository.java
@@ -0,0 +1,21 @@
+package com.cydeo.reository;
+
+import com.cydeo.entity.Genre;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
+
+public interface GenreRepository extends JpaRepository<Genre, Long> {
+
+    // -----------------------JPQL QUERIES-----------------------//
+    //Write a JPQL query that returns all genres
+    @Query("SELECT g FROM Genre g")
+    List<Genre> retrieveAllGenres();
+
+    //----------------------Native Queries------------------------//
+    //Write a native query that returns genres by containing name
+    @Query(value = "SELECT * FROM genre WHERE name ILIKE concat('%',?1,'%')", nativeQuery = true)
+    List<Genre> retrieveByName(@Param("name") String name);
+}
