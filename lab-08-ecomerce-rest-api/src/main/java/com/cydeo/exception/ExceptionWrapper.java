baad4aa4a91fc8b0a899e9ea93a06d790154b3b3
Kadifa <Kudickadifa@gmail.com>
2024-01-04 21:49:12 -0600

    Rest api practice

--- lab-08-ecomerce-rest-api/src/main/java/com/cydeo/exception/ExceptionWrapper.java
+++ lab-08-ecomerce-rest-api/src/main/java/com/cydeo/exception/ExceptionWrapper.java
@@ -1,13 +1,23 @@
 package com.cydeo.exception;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Getter;
 import lombok.Setter;
 import org.springframework.http.HttpStatus;
+
+import java.util.List;
+
 @Getter
 @Setter
+@JsonInclude(JsonInclude.Include.NON_NULL)
 public class ExceptionWrapper { // this is my costume class
     public String message;
     public HttpStatus httpStatus;
+
+    // additional fields for validation error class
+    private Integer errorCount;
+    private List<ValidationError> validationErrorList;// to keep the error count and put in a list of information inside the response wrapper
+
 
     // create constructor for message and httpStatus
 
