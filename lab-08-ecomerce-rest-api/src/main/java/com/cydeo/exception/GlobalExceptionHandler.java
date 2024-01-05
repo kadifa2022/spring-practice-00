dcdd27e1fe59ede6aecbc27e461d71b925e7683c
Kadifa <Kudickadifa@gmail.com>
2024-01-04 21:49:22 -0600

    Rest api practice

--- lab-08-ecomerce-rest-api/src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
+++ lab-08-ecomerce-rest-api/src/main/java/com/cydeo/exception/GlobalExceptionHandler.java
@@ -2,8 +2,14 @@ package com.cydeo.exception;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.validation.ObjectError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @RestControllerAdvice  // To capture all exception
 public class GlobalExceptionHandler {
@@ -13,5 +19,34 @@ public class GlobalExceptionHandler {
     ExceptionWrapper exceptionWrapper = new ExceptionWrapper(ex.getMessage(), HttpStatus.NOT_FOUND);
     return ResponseEntity.status(HttpStatus.NOT_FOUND).body(exceptionWrapper);
     }
+    @ExceptionHandler(MethodArgumentNotValidException.class)// what ever this error happen return this.
+    public ResponseEntity<ExceptionWrapper> processValidationError(MethodArgumentNotValidException ex){ // accepting
+        return handleMethodArgumentNotValid(ex);
+    }
+    protected ResponseEntity<ExceptionWrapper> handleMethodArgumentNotValid(MethodArgumentNotValidException ex){
+        ExceptionWrapper exceptionWrapper = new ExceptionWrapper("Invalid Input", HttpStatus.BAD_REQUEST);
+        List<ValidationError> validationErrors = new ArrayList<>();
+
+        for(ObjectError error : ex.getBindingResult().getAllErrors()){
+
+            String fieldName= ((FieldError) error).getField();
+            Object rejectedValue =((FieldError) error).getRejectedValue();
+            String errorMessage = error.getDefaultMessage();
+
+            ValidationError validationError = new ValidationError(fieldName, rejectedValue,errorMessage);
+            validationErrors.add(validationError);
+        }
+        exceptionWrapper.setValidationErrorList(validationErrors);
+        exceptionWrapper.setErrorCount(validationErrors.size());
+        return new ResponseEntity<>(exceptionWrapper,exceptionWrapper.getHttpStatus());
+    }
+    @ExceptionHandler({Exception.class, RuntimeException.class, Throwable.class})
+    public ResponseEntity<ExceptionWrapper> genericExceptionHandler(){
+        return new ResponseEntity<>(new ExceptionWrapper("Action failed: An error occurred!", HttpStatus.INTERNAL_SERVER_ERROR),HttpStatus.INTERNAL_SERVER_ERROR);
+
+    }
+
+
+
 
 }
